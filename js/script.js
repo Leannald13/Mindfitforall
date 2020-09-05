e35db3e3d8b5bad1497da41efafaa1196c306ab1
Leannald13 <leannald13@gmail.com>
2020-09-05 21:28:27 +0000

    changed navbar and added image carousel for testimonials

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+var contentString = '<h5>Potton, Sandy, SG19 UK <br>contact@mindfitforall.org<br>07841332552</h5>'
 
 function initMap () {
     var options = {
@@ -11,5 +11,14 @@ function initMap () {
     var marker = new google.maps.Marker({
         position: {lat: 52.127066,lng: -0.215195},
         map:map
-    })
+    
+    });
+
+    var infoWindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    marker.addListener('click', function() {
+        infoWindow.open(map, marker);
+    });
 }
